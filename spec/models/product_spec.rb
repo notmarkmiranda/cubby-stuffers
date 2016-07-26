645009ca4399fde8106f9068fcd965ce0060d0ae
Brian Sayler <sayler.b@gmail.com>
2016-07-25 19:33:26 -0600

    add simple spec for item_names method

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "lists associated item names" do
+    product = create(:product)
+    item1 = create(:item, name: "Peanut M&M's")
+    item2 = create(:item, name: "Wheat Thins")
+    product.items << [item1, item2]
+
+    expect(product.item_names).to eq("Peanut M&M's, Wheat Thins")
+  end
 end
