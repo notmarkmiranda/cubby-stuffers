10ee33e0cfe0bfe2bf38a95f6b19cc49d81aa5ee
Charles Kaminer <ckamine@g.clemson.edu>
2016-07-25 20:00:57 -0600

    write feature test for visitor create account

---
+++ spec/features/visitor_create_account_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "visitor can create an internal account" do
+  context "valid credentials" do
+    scenario "they are redirected to the user dashboard" do
+      visit root_path
+
+      click_on "Login"
+
+      expect(current_path).to eq login_path
+
+      click_on "Create Account"
+
+      expect(current_path).to eq new_user_path
+
+      fill_in "First Name", with: "Neight"
+      fill_in "Last Name", with: "Aeighlen"
+      fill_in "Email", with: "neightaeighlen@test.com"
+      fill_in "Email Confirmation", with: "neightaeighlen@test.com"
+      fill_in "Password", with: "password"
+      fill_in "Password Confirmation", with: "password"
+      fill_in "Phone Number", with: "123-456-7890"
+      click_on "Create Account"
+
+      expect(current_path).to eq dashboard_path
+      expect(page).to have_content "Welcome, Neight!"
+    end
+  end
+end
