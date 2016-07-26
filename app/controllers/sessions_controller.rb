52793326b2505ecde2a30ca034c90b2ce249c39c
Brian Sayler <sayler.b@gmail.com>
2016-07-26 16:16:03 -0600

    add logout spec
    -added checking request referer for nil value
    -added a destroy method to the sessions controller
    -changed method of logout link in layout to 'delete'
    -deleted all the commented stuff in the login spec
    -added a logout spec
    -moved login_user method to the rails_helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,22 @@
 class SessionsController < ApplicationController
   def new
-    session[:last_page] = request.referer
+    session[:last_page] = request.referer 
   end
 
   def create
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to session[:last_page]
+      session[:last_page] ? redirect_to(session[:last_page]) : redirect_to(login_path)
     else
       flash.now[:error] = "Invalid credentials"
       render :new
     end
+  end
+
+  def destroy
+    session.clear
+    flash[:notice] = "Logged out successfully!"
+    redirect_to login_path
   end
 end
