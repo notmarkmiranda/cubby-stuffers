d7d8e4c398647cdf147effc5873c50f1fc039936
Charles Kaminer <cman1310@yahoo.com>
2016-07-26 08:50:39 -0600

    4 visitor create account (#10)
    * write feature test for visitor create account
    
    * implement create account user story
    write create account feature test
    add current_user method to application controller
    create sessions controller with new action
    create users controller with new create show actions
    generate user model and migrate to db
    create navbar with login button
    add email and password confirmation validations to user model
    add create account button to new session view
    create new user form
    write create user action with strong params
    create custom dashboard route to user show
    create login route to sessions new

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome To Cubby Stuffers!"
+      redirect_to dashboard_path
+    else
+      flash.now[:error] = @user.errors.full_messages.join(", ")
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name,
+                                   :email, :email_confirmation,
+                                   :password, :password_confirmation,
+                                   :phone_number)
+    end
+end
