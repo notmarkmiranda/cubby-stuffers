fde37897aa6e646e5cd7acbb062326ee3d3329d4
Brian Sayler <saylerb@users.noreply.github.com>
2016-08-03 21:21:25 -0600

    convert price comparison tool to AJAX (#62)
    - write ajax javascript file
    - add css for loading spinner
    - delete old price comparison controller
    - delete old feature spec for price comparison
    - edit price comparison view
    - add quantities to amazon product (for unit price calculation)
    - add a link to the price comparison tool from admin dashboard

--- app/controllers/api/v1/price_comparison_controller.rb
+++ app/controllers/api/v1/price_comparison_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::PriceComparisonController < ApiBaseController
 
   def index
+    # binding.pry
     walmart_products = WalmartProductSearch.get_all(params[:query])
     query = params[:query]
     upcs_array = walmart_products.map(&:upc)
